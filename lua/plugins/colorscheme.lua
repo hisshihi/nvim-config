c80a435603c8b8c975adc272ca959c34f62f662f
hisshihi <hissoffc@gmail.com>
2025-06-03 17:56:22 +0500

    Add plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,1 @@
--- lua/plugins/rose-pine.lua
-return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    vim.cmd("colorscheme rose-pine-moon")
-  end,
-}
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
